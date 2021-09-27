865f331bc94dc50301e048e2b2fb07d7275c6689
Sayyidusy <sayyidusyalghiffari@gmail.com>
2021-09-27 17:06:10 +0700

    HelloWorld.java

---
+++ coba/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+
+	public static void main (String[] args)	{
+		System.out.println ("Nama : Sayyidusy");
+        System.out.println ("NIM  : 6706213149");
+        System.out.println ("Kelas: 4503");
+	}	
+} 
